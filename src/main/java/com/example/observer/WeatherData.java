31106221103a5b3f2ea560eb064d8b7f910ccd74
Raushan <raushankumar606@gmail.com>
2023-12-18 10:22:20 +0530

    weatherdata

--- src/main/java/com/example/observer/WeatherData.java
+++ src/main/java/com/example/observer/WeatherData.java
@@ -1,14 +1,42 @@
 package com.example.observer;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class WeatherData {
 
-    public void measurementsChanged() {
-        float temp = getTemperature();
-        float humidity = getHumidity();
-        float pressure = getPressure();
+    List<Observer> observers;
+    private float temperature;
+    private float humidity;
+    private float pressure;
 
-        currentConditionsDisplay.update(temp, humidity, pressure);
-        staticsDisplay.update(temp, humidity, pressure);
-        forecastDisplay.update(temp, humidity, pressure);
+    public WeatherData() {
+        observers = new ArrayList<>();
     }
+
+    public void registerObserver(Observer o) { observers.add(o); }
+    
+    public void removeObserver(Observer o) {
+        int i = observers.indexOf(o);
+        if (i >= 0) { observers.remove(i); }
+    }
+
+    public void notifyObservers() {
+        for (Observer observer : observers) {
+            observer.update(temperature, humidity, pressure);
+        }
+    }
+
+
+    public void measurementsChanged() {
+        notifyObservers();
+    }
+
+    public void setMeasurements(float temperature, float humidity, float pressure) {
+        this.temperature = temperature; 
+        this.humidity = humidity;
+        this.pressure = pressure;
+        measurementsChanged();
+    }
+    
 }
